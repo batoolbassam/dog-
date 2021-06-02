e9748e0523e066b60b19a871453e25a66f1f7df6
BatoolBassam <>
2021-06-02 18:07:39 +0000

    ,

---
+++ app.js
@@ -0,0 +1,15 @@
+alert(welcome)
+var doganswer = prompt("do you like dogs?")
+alert(''thankfully '+ doganswer)
+document.write('<div>' + ''hello' + '</div>)
+if ( doganswer==''yes') {
+  document.write('<h3>' + doganswer + '</h3>')
+  }
+  else if ( dogandwer==''no'){
+    document.write('<h3>' + doganswer + '</h3>')
+    }
+  
+else {
+  alert(''please type a correct answe')
+  
+}
