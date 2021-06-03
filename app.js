ab12889912c662d438f8f97b87b4e5771c38d89d
BatoolBassam <>
2021-06-03 16:07:46 +0000

    ,

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-<<<<<<< HEAD
+
 alert("welcome")
-=======
-alert(welcome)''
->>>>>>> 100b48b3c93b144dc03ea7186f3d5d1ac77e917b
+
 var doganswer = prompt("do you like dogs?")
 alert("thankfully" + doganswer )
-document.write('<div>' + "hello" + '</div>')
+ 
+ document.write('<h3>' + "are you a dog lover" + '</h3>')
+ 
+ 
 if (doganswer== 'yes') {
   document.write('<h3>' + doganswer + '</h3>')
   }
@@ -20,3 +21,33 @@ else {
   
   
 }
+var x = prompt("how many dogs do you like?");
+if(x >4 ){
+  alert("this is a huge number")
+  document.write('<h6>' + "oh great you like" + x + '</h6>')
+  
+  
+}
+else
+{
+  for(var d = 1 ; d <= x;d++){
+    document. write("<h7>"+"woof"+ "</h7>")
+    console.log(d)
+  }
+}
+
+document.write('<div>' + "guess what is the most loving dog breed" +'</div>')
+var guess = "golden retriever"
+    var ask = prompt ("guess what is the most loving dog breed");
+    var attempt = 2;
+    for(var i = 0 ; i< attempt;i++){
+      if(ask == guess){
+        break;
+        
+      }
+      
+      ask = prompt ("guess what is the most loving dog breed");
+      
+    }
+document.write('<h6>' + "golden retriever" +'</h6>')
+
